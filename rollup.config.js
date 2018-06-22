a83e5630155312c1ecb1e70f981988c348abcd85
Eric T Grubaugh <eric@stoic.software>
2018-06-22 13:05:38 -0600

    Relocated jsdoc header to rollup banner

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,16 @@ export default {
     output: [{
         file: "dist/shaped5e-util.js",
         format: "iife",
-        name: "ShapedUtil"
+        name: "ShapedUtil",
+        banner: `
+/**
+ * Utility methods and constants for the Shaped 5e Character Sheet in Roll20
+ *
+ * @namespace
+ *
+ * @author Eric T Grubaugh
+ * @copyright 2018 Zone & Co
+ * @license MIT
+ */`
     }]
 }
--- src/shaped5e-util.js
+++ src/shaped5e-util.js
@@ -1,14 +1,3 @@
-/**
- * Utility methods and constants for the Shaped 5e Character Sheet in Roll20
- *
- * @module shaped5e/util
- *
- * @author Eric T Grubaugh
- * @copyright 2018 Zone & Co
- * @license MIT
- */
-
-
 on("ready", () => { log("Shaped 5e Utils loaded."); });
 
 export * from "./shaped5e-skills";
